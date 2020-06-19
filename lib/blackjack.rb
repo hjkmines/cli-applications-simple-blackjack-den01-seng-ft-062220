f835a4d51bf5f651b8ce9d5c82fd53da7b1aadee
Hyung Kim <hjkmines@gmail.com>
2020-06-19 04:41:58 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,6 +51,8 @@ def hit?(number)
     number 
   else
     invalid_command
+    prompt_user
+    get_user_input
   end 
   
 end
@@ -58,8 +60,6 @@ end
 def invalid_command
   # code invalid_command here
   puts "Please enter a valid command"
-  prompt_user
-  get_user_input
 end
 
 #####################################################
