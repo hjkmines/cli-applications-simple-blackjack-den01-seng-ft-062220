28b6b3443dc0ce8c911394db845d4b0c5e1da0fb
Hyung Kim <hjkmines@gmail.com>
2020-06-19 03:33:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,8 +25,9 @@ def get_user_input
   gets.chomp
 end
 
-def end_game
+def end_game(number)
   # code #end_game here
+  puts "Sorry, you hit #{number}. Thanks for playing!"
 end
 
 def initial_round
